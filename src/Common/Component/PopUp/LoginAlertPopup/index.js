d2193a4f2861997e009663dee38cb6e960d8477e
christina <>
2022-01-26 18:31:16 +0900

    clear code

--- src/Common/Component/PopUp/LoginAlertPopup/index.js
+++ src/Common/Component/PopUp/LoginAlertPopup/index.js
@@ -1,24 +1,5 @@
 import React, { Component } from "react";
 import "./index.scss";
-import styled from "@emotion/styled";
-
-// const CancelButton = styled.button`
-//   width: 370px;
-//   height: 50px;
-//   border-radius: 10px;
-//   background-color: #1c1c1c;
-//   border: none;
-//   font-size: 20px;
-//   font-weight: bold;
-//   line-height: 1.2;
-//   letter-spacing: 0.38px;
-//   cursor: pointer;
-//   color: #ef7851;
-
-//   &:focus {
-//     outline: none;
-//   }
-// `
 
 const LoginAlertPopup = ({ message, onSubmit, ...props }) => {
   const { dismiss } = props;
@@ -31,7 +12,6 @@ const LoginAlertPopup = ({ message, onSubmit, ...props }) => {
         <button className="popup-login-alert-confirm" onClick={async () => {
           dismiss();
         }}>Confirm</button>
-        {/* <CancelButton onClick={() => dismiss()}>취소</CancelButton> */}
       </div>
     </div>
   );
