ef212fd09c14dd5024e563cf3f6de9c5b97383e1
christina <>
2022-01-28 17:38:51 +0900

    layout edit

--- src/Page/LMS/Sections/Dashboard.js
+++ src/Page/LMS/Sections/Dashboard.js
@@ -502,7 +502,6 @@ const GraphProgressPercent = styled.div`
 const CircularWrap = styled.div`
   display: flex;
   align-items: center;
-  justify-content: flex-start;
   flex-flow: row wrap;
   flex: 1;
   
