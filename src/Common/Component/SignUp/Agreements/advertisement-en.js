9b9d67fb4b5e248664446509dfbc5d5a468ace57
christina <>
2022-02-08 16:31:49 +0900

    add translation for advertisement agreement

---
+++ src/Common/Component/SignUp/Agreements/advertisement-en.js
@@ -0,0 +1,8 @@
+export default `### **Consent to collect and use personal information for advertising information transmission (optional)**
+
+1. Purpose of collection: **Use of marketing such as product information, new packages, participation in experience events, etc. (Sending advertising information)**
+2. Collection of information: student name, grade, mobile phone number.
+3. Collection and usage period: **Until withdrawal of consent.**
+
+You have the right to refuse consent and can still use the service from Redbrick. However, you may not be able to receive useful information through newsletters.
+`;
