214122bbd9ada8701c66bed512764d22fd9187b6
christina <>
2022-03-16 14:50:18 +0900

    add oobc course tutorial ja and en guide

--- src/Page/Builder/Component/DreamBuilder/Tutorial/index.js
+++ src/Page/Builder/Component/DreamBuilder/Tutorial/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { injectIntl } from "react-intl";
-import { jsScript, oobcScript } from "./script";
+import { jsScript, oobcScript, oobcScript_en, oobcScript_ja } from "./script";
 import "./index.scss";
 
 class Tutorial extends Component {
@@ -10,43 +10,52 @@ class Tutorial extends Component {
     this.state = {
       scriptIndex: 0
     };
+
   }
 
   get script() {
-    if(this.props.editorMode === "block") {
-      return oobcScript;
+    const lang = localStorage.getItem("lang");
+    console.log(1111, lang)
+    if (this.props.editorMode === "block") {
+      if (lang == "ja") {
+        return oobcScript_ja;
+      } else if (lang === "en") {
+        return oobcScript_en;
+      } else {
+        return oobcScript;
+      }
     } else {
       return jsScript;
     }
   }
 
   onClickReset = () => {
-    this.setState({ scriptIndex : 0 })
+    this.setState({ scriptIndex: 0 })
   }
-  
+
   onClickNext = () => {
-    this.setState({ scriptIndex : this.state.scriptIndex + 1 })
+    this.setState({ scriptIndex: this.state.scriptIndex + 1 })
   }
 
   onClickEnd = () => {
     localStorage.setItem(`didShowTutorial_${this.props.editorMode}`, true);
     this.props.hiddenTutorial();
   }
-  
+
   render() {
     return (
       <div className="tutorial">
         <div className="tutorial_popup">
           <p className="guideLabel"><span className="num">{this.state.scriptIndex + 1}</span>{this.script[this.state.scriptIndex].subTitle}</p>
           <p className="guideTitle">{this.script[this.state.scriptIndex].title}</p>
-          <img className="guideImg" src={this.script[this.state.scriptIndex].imageURL} alt="guide"/>
+          <img className="guideImg" src={this.script[this.state.scriptIndex].imageURL} alt="guide" />
           <div className="buttonWrapper">
             {this.state.scriptIndex === this.script.length - 1 ? (
               <>
                 <button className="reset" onClick={this.onClickReset} >{this.props.intl.formatMessage({ id: "ID_DREAM_BUILDER_TUTORIAL_RESET" })}</button>
                 <button onClick={this.onClickEnd}>{this.props.intl.formatMessage({ id: "ID_DREAM_BUILDER_TUTORIAL_START" })}</button>
-              </> 
-              ) : ( 
+              </>
+            ) : (
               <button onClick={this.onClickNext}>{this.props.intl.formatMessage({ id: "ID_DREAM_BUILDER_TUTORIAL_NEXT" })}</button>
             )}
           </div>
--- src/Page/Builder/Component/DreamBuilder/Tutorial/script.js
+++ src/Page/Builder/Component/DreamBuilder/Tutorial/script.js
@@ -3,6 +3,19 @@ import woT02 from "../../../../../Image/dreamclass/builder_tutorial/wo-tutorial-
 import woT03 from "../../../../../Image/dreamclass/builder_tutorial/wo-tutorial-03@2x.png";
 import woT04 from "../../../../../Image/dreamclass/builder_tutorial/wo-tutorial-04@2x.png";
 import woT05 from "../../../../../Image/dreamclass/builder_tutorial/wo-tutorial-05@2x.png";
+
+import woT01_en from "../../../../../Image/dreamclass/builder_tutorial/en-wo-tutorial-1.png";
+import woT02_en from "../../../../../Image/dreamclass/builder_tutorial/en-wo-tutorial-2.png";
+import woT03_en from "../../../../../Image/dreamclass/builder_tutorial/en-wo-tutorial-3.png";
+import woT04_en from "../../../../../Image/dreamclass/builder_tutorial/en-wo-tutorial-4.png";
+import woT05_en from "../../../../../Image/dreamclass/builder_tutorial/en-wo-tutorial-5.png";
+
+import woT01_jp from "../../../../../Image/dreamclass/builder_tutorial/ja-wo-tutorial-01.png";
+import woT02_jp from "../../../../../Image/dreamclass/builder_tutorial/ja-wo-tutorial-02.png";
+import woT03_jp from "../../../../../Image/dreamclass/builder_tutorial/ja-wo-tutorial-03.png";
+import woT04_jp from "../../../../../Image/dreamclass/builder_tutorial/ja-wo-tutorial-04.png";
+import woT05_jp from "../../../../../Image/dreamclass/builder_tutorial/ja-wo-tutorial-05.png";
+
 import wjT01 from "../../../../../Image/dreamclass/builder_tutorial/js-tutorial-01@2x.png";
 import wjT02 from "../../../../../Image/dreamclass/builder_tutorial/js-tutorial-02@2x.png";
 import wjT03 from "../../../../../Image/dreamclass/builder_tutorial/js-tutorial-03@2x.png";
@@ -10,17 +23,47 @@ import wjT04 from "../../../../../Image/dreamclass/builder_tutorial/js-tutorial-
 import wjT05 from "../../../../../Image/dreamclass/builder_tutorial/js-tutorial-05@2x.png";
 
 export const jsScript = [
-  { subTitle: "가이드탭", title: "도움이 필요하다면 여기서!", imageURL: wjT01}, 
-  { subTitle: "미션 리스트", title: "미션 진행 상황을 보고 싶으신가요?", imageURL: wjT02},
-  { subTitle: "스프라이트 / 사운드", title: "스프라이트가 뭐예요?", imageURL: wjT03}, 
-  { subTitle: "코드 입력", title: "코드를 입력해 볼까요?", imageURL: wjT04}, 
-  { subTitle: "편리 기능", title: "코딩을 좀 더 편하게", imageURL: wjT05}
+  { subTitle: "가이드탭", title: "도움이 필요하다면 여기서!", imageURL: wjT01 },
+  { subTitle: "미션 리스트", title: "미션 진행 상황을 보고 싶으신가요?", imageURL: wjT02 },
+  { subTitle: "스프라이트 / 사운드", title: "스프라이트가 뭐예요?", imageURL: wjT03 },
+  { subTitle: "코드 입력", title: "코드를 입력해 볼까요?", imageURL: wjT04 },
+  { subTitle: "편리 기능", title: "코딩을 좀 더 편하게", imageURL: wjT05 }
 ];
+// export const jsScript_en = [
+//   { subTitle: "Guide Tab", title: "Click here for help!", imageURL: wjT01}, 
+//   { subTitle: "Mission List", title: "Would you like to see your mission progress?", imageURL: wjT02},
+//   { subTitle: "Sprite / Sound", title: "What's a sprite?", imageURL: wjT03}, 
+//   { subTitle: "Enter Code", title: "Enter a code.", imageURL: wjT04}, 
+//   { subTitle: "Convenient Functions", title: "Code Easily", imageURL: wjT05}
+// ];
+// export const jsScript_ja = [
+//   { subTitle: "가이드탭", title: "도움이 필요하다면 여기서!", imageURL: wjT01}, 
+//   { subTitle: "미션 리스트", title: "미션 진행 상황을 보고 싶으신가요?", imageURL: wjT02},
+//   { subTitle: "스프라이트 / 사운드", title: "스프라이트가 뭐예요?", imageURL: wjT03}, 
+//   { subTitle: "코드 입력", title: "코드를 입력해 볼까요?", imageURL: wjT04}, 
+//   { subTitle: "편리 기능", title: "코딩을 좀 더 편하게", imageURL: wjT05}
+// ];
 
 export const oobcScript = [
-  { subTitle: "가이드탭", title: "도움이 필요하다면 여기서!", imageURL: woT01}, 
-  { subTitle: "미션 리스트", title: "미션 진행 상황을 보고 싶으신가요?", imageURL: woT02},
-  { subTitle: "스프라이트 / 사운드", title: "스프라이트가 뭐예요?", imageURL: woT03}, 
-  { subTitle: "코드 입력", title: "코드를 입력해 볼까요?", imageURL: woT04}, 
-  { subTitle: "편리 기능", title: "코딩을 좀 더 편하게", imageURL: woT05}
+  { subTitle: "가이드탭", title: "도움이 필요하다면 여기서!", imageURL: woT01 },
+  { subTitle: "미션 리스트", title: "미션 진행 상황을 보고 싶으신가요?", imageURL: woT02 },
+  { subTitle: "스프라이트 / 사운드", title: "스프라이트가 뭐예요?", imageURL: woT03 },
+  { subTitle: "코드 입력", title: "코드를 입력해 볼까요?", imageURL: woT04 },
+  { subTitle: "편리 기능", title: "코딩을 좀 더 편하게", imageURL: woT05 }
+];
+
+export const oobcScript_en = [
+  { subTitle: "Guide Tab", title: "Click here for help!", imageURL: woT01_en },
+  { subTitle: "Mission List", title: "Would you like to see your mission progress?", imageURL: woT02_en },
+  { subTitle: "Sprite / Sound", title: "What's a sprite?", imageURL: woT03_en },
+  { subTitle: "Enter Code", title: "Enter a code.", imageURL: woT04_en },
+  { subTitle: "Convenient Functions", title: "Code Easily", imageURL: woT05_en }
+];
+
+export const oobcScript_ja = [
+  { subTitle: "ガイドタブ", title: "ヘルプが必要ならここ！", imageURL: woT01_jp },
+  { subTitle: "ミッションリスト", title: "ミッションの進行状況を見たいですか？", imageURL: woT02_jp },
+  { subTitle: "スプライト / サウンド", title: "スプライトとは何ですか？", imageURL: woT03_jp },
+  { subTitle: "コード入力", title: "コードを入力してみましょう。", imageURL: woT04_jp },
+  { subTitle: "便利機能", title: "コーディングをもっと楽に", imageURL: woT05_jp }
 ];
