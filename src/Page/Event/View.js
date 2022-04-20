9db9b694ef0ca9761926839e932f319986d7f55e
haL-redbrick <hal@redbrick.space>
2022-04-20 14:15:00 +0900

    컴포넌트화 및 UI 디테일 수정

--- src/Page/Event/View.js
+++ src/Page/Event/View.js
@@ -28,59 +28,11 @@ export default function View(props) {
 
           <div className="account__content-wrap">
             <form action="">
-              <div className="account__content-list">
-                {/* TODO 계정관리 UI */}
-                <div className="account__content-item">
-                  <span className="account__content-category">이름</span>
-                  <input type="text" className="account__content-input" placeholder="이름을 입력하세요." />
-                  <button type="button" className="account__edit-btn">수정</button>
-                </div>
-
-                <div className="account__content-item">
-                  <span className="account__content-category">닉네임</span>
-                  <input type="text" className="account__content-input" placeholder="닉네임을 입력하세요." />
-                  <button type="button" className="account__edit-btn">수정</button>
-                </div>
-                {/* // TODO 계정관리 UI */}
-
-                {/* TODO 비밀번호 변경 UI */}
-                {/* TODO : 현재 비밀번호 input */}
-                <div className="account__content-item">
-                  <span className="account__content-category" id="test">현재 비밀번호</span>
-                  <div className="account__password-change">
-                    <input type="password" className="account__content-input account__content-input--password" placeholder="현재 비밀번호를 입력해주세요." />
-                    <button className="account__password-show">
-                      <img src={EyesOffIcon} alt="비밀번호 안보기 아이콘" /> 
-                    </button>
-                  </div>
-                </div>
-                {/* TODO : 새 비밀번호 input */}
-                <div className="account__content-item">
-                  <span className="account__content-category" >새 비밀번호</span>
-                  <div className="account__password-change">
-                    <input type="password" className="account__content-input account__content-input--password" placeholder="새 비밀번호를 입력해주세요." />
-                    <button className="account__password-show">
-                      <img src={EyesOnIcon} alt="비밀번호 보기 아이콘" /> 
-                    </button>
-                  </div>
-                </div>
-                {/* TODO : 새 비밀번호 확인 input */}
-                <div className="account__content-item">
-                  <span className="account__content-category">새 비밀번호 확인</span>
-                  <div className="account__password-change">
-                    <input type="password" className="account__content-input account__content-input--password" placeholder="새 비밀번호를 입력해주세요." />
-                    <button className="account__password-show">
-                      <img src={EyesOnIcon} alt="비밀번호 보기 아이콘" /> 
-                    </button>
-                  </div>
-                </div>
-                {/* // TODO 비밀번호 변경 UI */}
-              </div>
-
-              <div className="account__password-changed-btn-box">
-                {/* TODO : 활성화 시 클래스 active 추가 */}
-                <button type="submit" className="account__password-changed-btn">비밀번호 변경</button>
-              </div>
+              {/* TODO 계정관리 */}
+              {/* {true ? <ChangeName /> : <ChangePassword />} */}
+              
+              {/* TODO 비밀번호 변경 */}
+              {false ? <ChangeName /> : <ChangePassword />}
             </form>
           </div>
         </div>
@@ -89,3 +41,64 @@ export default function View(props) {
   );
 }
 
+const ChangeName = () => {
+  return (
+    <>
+    <div className="account__content-item">
+      <span className="account__content-category">이름</span>
+      <input type="text" className="account__content-input" autoComplete="off" placeholder="이름을 입력하세요." />
+      <button type="button" className="account__edit-btn">수정</button>
+    </div>
+
+    <div className="account__content-item">
+      <span className="account__content-category">닉네임</span>
+      <input type="text" className="account__content-input" autoComplete="off" placeholder="닉네임을 입력하세요." />
+      <button type="button" className="account__edit-btn">수정</button>
+    </div>
+    </>
+  )
+}
+
+const ChangePassword = () => {
+  return (
+    <>
+    <div className="account__content-list">
+      <div className="account__content-item">
+        <span className="account__content-category" id="test">현재 비밀번호</span>
+        <div className="account__password-change">
+          <input type="password" autoComplete="off" className="account__content-input account__content-input--password" placeholder="현재 비밀번호를 입력해주세요." />
+          <button className="account__password-show">
+            <img src={EyesOffIcon} alt="비밀번호 안보기 아이콘" /> 
+          </button>
+        </div>
+      </div>
+      {/* TODO : 새 비밀번호 input */}
+      <div className="account__content-item">
+        <span className="account__content-category" >새 비밀번호</span>
+        <div className="account__password-change">
+          <input type="password" autoComplete="off" className="account__content-input account__content-input--password" placeholder="새 비밀번호를 입력해주세요." />
+          <button className="account__password-show">
+            <img src={EyesOnIcon} alt="비밀번호 보기 아이콘" /> 
+          </button>
+        </div>
+      </div>
+      {/* TODO : 새 비밀번호 확인 input */}
+      <div className="account__content-item">
+        <span className="account__content-category">새 비밀번호 확인</span>
+        <div className="account__password-change">
+          <input type="password" autoComplete="off" className="account__content-input account__content-input--password" placeholder="새 비밀번호를 입력해주세요." />
+          <button className="account__password-show">
+            <img src={EyesOnIcon} alt="비밀번호 보기 아이콘" /> 
+          </button>
+        </div>
+      </div>
+      {/* // TODO 비밀번호 변경 UI */}
+      
+    </div>
+    <div className="account__password-changed-btn-box">
+      {/* TODO : 활성화 시 클래스 active 추가 */}
+      <button type="submit" className="account__password-changed-btn">비밀번호 변경</button>
+    </div>
+  </>
+  )
+}
