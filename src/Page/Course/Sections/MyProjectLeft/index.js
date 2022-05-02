8c2233143b742ed4fd183e746c7372f2bddd6262
haL-redbrick <hal@redbrick.space>
2022-05-03 08:43:14 +0900

    rebuild

--- src/Page/Course/Sections/MyProjectLeft/index.js
+++ src/Page/Course/Sections/MyProjectLeft/index.js
@@ -8,13 +8,12 @@ import "./index.scss";
 function MyProjectLeft () {
   return (
     <div className="LeftSlide">
-      { true ? <BeforeLogin /> : <AfterLogin /> }
+      { false ? <BeforeLogin /> : <AfterLogin /> }
     </div>
   )
 }
 
 export default MyProjectLeft ;
-
 
 /* TODO 로그인 전 슬라이드 배너 */
 const BeforeLogin = () => {
@@ -70,24 +69,51 @@ const BeforeLogin = () => {
 
 /* TODO 로그인 후 슬라이드 배너 */
 const AfterLogin = () => {
-  return (
-    <div className="content-slide">
-      <span className="content-slide__course-number">Coures 1</span>
-      <h3 className="content-slide__course-title">OOBC - Elementary</h3>
+  const [swiper, setSwiper] = React.useState(null);
 
+  const nexTo = () => {
+    swiper.slideNext();
+  };
+
+  const prevTo = () => {
+    swiper.slidePrev();
+  };
+  return (
+   <>
+    <h3 className="page-title">원하는 코스를 무료로 체험해보세요 두번째</h3>
+    <div className="content-slide">
       <div className="content-slide__slide">
         <Swiper 
           spaceBetween={15}
           slidesPerView={1}
-          navigation
-          onSwiper={(swiper) => console.log(swiper)}
+          pagination={{ clickable: true }}
           onSlideChange={() => console.log('slide change')}
+          onSwiper={(s) => {
+            setSwiper(s);
+          }}
         >
-          <SwiperSlide>slide1</SwiperSlide>
+          <SwiperSlide>
+            {/* TODO course 횟수 */}
+            <span className="content-slide__course-number">Coures 1</span>
+            {/* TODO 강의명 */}
+            <h3 className="content-slide__course-title">OOBC - Elementary</h3>
+            <div className="content-slide__thumbnail-wrap">
+              {/* TODO 썸네일 */}
+              <img alt="" src="" />
+            </div>
+            <p className="content-slide__explan">
+              Learns for students new to coding. Learn to code by creating simple games with block coding. 최대 3줄 까지.
+            </p>
+            <button type="button" className="content-slide__experience-btn">Experience</button>
+          </SwiperSlide>
           <SwiperSlide>slide2</SwiperSlide>
         </Swiper>
       </div>
+
+      <button type="button" className="swiper-button-next" onClick={nexTo} />
+      <button type="button" className="swiper-button-prev" onClick={prevTo} />
     </div>
+   </>
   )
 }
 
