68847065cb23756f270124cdd8f3ddf13c4482c1
haL-redbrick <hal@redbrick.space>
2022-05-23 14:45:30 +0900

    tab

--- src/Page/Intro/PlayGameSection/index.js
+++ src/Page/Intro/PlayGameSection/index.js
@@ -7,7 +7,7 @@ function PlayGameSection () {
     <div className="game-video">
       <h3 className="game-video__title"><FormattedMessage id="ID_INTRO_GAME_VIDEO_SECTION_TITLE" /></h3>
       <span className="game-video__child-title">
-      <FormattedMessage id="ID_INTRO_GAME_VIDEO_SECTION_CHILD_TITLE" />
+        <FormattedMessage id="ID_INTRO_GAME_VIDEO_SECTION_CHILD_TITLE" />
       </span>
 
       <div className="game-video__outline">
@@ -16,9 +16,11 @@ function PlayGameSection () {
 
         {/* 상단 how to play 영역 */}
         <div className="game-video__how-to-play">
-          <span className="game-video__how-to-play-left"><FormattedMessage id="ID_INTRO_GAME_VIDEO_LINE_BANNER_TITLE_LEFT" /></span>
+          <span className="game-video__how-to-play-left">
+            <FormattedMessage id="ID_INTRO_GAME_VIDEO_LINE_BANNER_TITLE_LEFT" />
+          </span>
           <span className="game-video__how-to-play-right">
-          <FormattedMessage id="ID_INTRO_GAME_VIDEO_LINE_BANNER_TITLE_RIGHT" />
+            <FormattedMessage id="ID_INTRO_GAME_VIDEO_LINE_BANNER_TITLE_RIGHT" />
           </span>
         </div>
       </div>
