90f39866d683c425e2ca2bbab32bdbc60d6a9e77
haL-redbrick <hal@redbrick.space>
2022-05-23 11:23:25 +0900

    설명 주석 추가

--- src/Page/Intro/LineBanner/index.js
+++ src/Page/Intro/LineBanner/index.js
@@ -9,6 +9,8 @@ function LineBanner () {
       <div className="line-banner__inner">
         <h3 className="line-banner__title"><FormattedMessage id="ID_INTRO_LINE_BANNER_TITLE" /></h3>
         <span className="line-banner__child-title"><FormattedMessage id="ID_INTRO_LINE_BANNER_CHILD_TITLE" /></span>
+
+        {/* TODO  클릭 시 무료 체험용 과정 페이지로 랜딩 됩니다. */}
         <a className="line-banner__go-to-link" rel="noopener noreferrer" ><FormattedMessage id="ID_INTRO_LINE_BANNER_GO_TO_FREE_COURSE_LINK" />
         <img alt="더보기 아이콘" src={LinkArrowRightIcon} /></a>
       </div>
