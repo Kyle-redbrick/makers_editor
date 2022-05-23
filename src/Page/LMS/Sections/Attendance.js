bee297554ec109b02b42545207bd0f815b36092a
christina <>
2022-05-23 15:24:12 +0900

    attendance UI

--- src/Page/LMS/Sections/Attendance.js
+++ src/Page/LMS/Sections/Attendance.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
 import styled from "@emotion/styled";
-import Calendar from 'react-calendar';
+import moment from "moment";
+import "moment/locale/ko";
+import "moment/locale/en-gb";
+import "moment/locale/ja";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 import { COLOR } from "../Constants";
 import nextArrow from "../../../Image/btn_right_arrow.svg"
 import prevArrow from "../../../Image/btn_left_arrow.svg"
@@ -16,34 +21,58 @@ const Self = styled.div`
 
 const Attendance = ({ ...props }) => {
   const [value, onChange] = useState(new Date());
-  console.log(value.getDate()+1)
+  console.log(value.getDate() + 1)
   const test = "Tue May 04 2022 09:16:24 GMT+0900 (Korean Standard Time"
 
-  // const [cards, setCards] = useState([]);
-
-  // useEffect(() => {
-  //   init();
-  // }, []);
-
-  // const init = async () => {
-  //   const _cards = await getInventoryCards({ userId: 1 });
-  //   setCards(_cards.map((card) => new InventoryCard(card)));
-  // };
+  moment.locale("en-US");
+  const localizer = momentLocalizer(moment);
+  localizer.formats.dateFormat = 'D';
+  const events = [];
 
   return (
     <Self {...props}>
-      <Calendar  
-        calendarType={"US"}
-        prevLabel={<img src={prevArrow}/>}
-        nextLabel={<img src={nextArrow} />}
-        value={[value,new Date().setDate(2)]} defaultView={"month"}
-        prev2Label={""}
-        next2Label={""}
-        showNeighboringMonth={false}
-        showFixedNumberOfWeeks={false}
+      <div className="attendance__title">Attendance</div>
+      <div className="attendance__calendar-container">
+        <Calendar
+          localizer={localizer}
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ minHeight: 711 }}
+          components={{
+            toolbar: Toolbar
+          }}
         />
+      </div>
     </Self>
   );
 };
+
+
+const Toolbar = ({ date, onNavigate }) => {
+  const locale = localStorage.getItem("lang");
+  const month = "월";
+  const year = '년';
+  const en_month = date.toLocaleString('en-US', { month: 'long' });
+  const navigate = (action) => {
+    onNavigate(action);
+  };
+
+  return (
+    <div className="rbc-toolbar">
+      <span className="rbc-btn-group">
+        <img src={prevArrow} alt="prev" onClick={navigate.bind(null, 'PREV')} />
+        <span className="rbc-toolbar-label">
+          {locale !== "en" ?
+            `${date.getFullYear()} ${month} ${date.getMonth() + 1} ${year}`
+            :
+            `${en_month} ${date.getFullYear()}`
+          }
+        </span>
+        <img src={nextArrow} alt="next" onClick={navigate.bind(null, 'NEXT')} />
+      </span>
+    </div>
+  );
+}
 
 export default Attendance;
