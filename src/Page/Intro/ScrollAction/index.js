87ee84c47bcd33ee72b912896cdb86ff2b0597c7
haL-redbrick <hal@redbrick.space>
2022-05-26 09:11:28 +0900

    scroll action 영역 생성

---
+++ src/Page/Intro/ScrollAction/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss";
+
+function ScrollAction () {
+  return (
+    <div className="scroll-action">
+      
+    </div>
+  )
+}
+
+export default ScrollAction ;
