3b6a3ba8cbf0e23cdf6580d4e4ba5a315cbace4f
rarlala-redbrick <rarla@redbrick.space>
2022-05-31 10:24:22 +0900

    change img

--- src/Page/About/HorizontalSlide/index.js
+++ src/Page/About/HorizontalSlide/index.js
@@ -2,13 +2,7 @@ import React from "react";
 import { FormattedMessage } from "react-intl";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper";
-import testImg from "../../../Image/test.png";
-import ImgVertical1 from "../../../Image/img-vertical-1.PNG";
-
 import ImgHorizontal1 from "../../../Image/img-horizontal-1.png";
-import ImgHorizontal2 from "../../../Image/img-horizontal-2.png";
-import ImgHorizontal3 from "../../../Image/img-horizontal-3.png";
-import ImgHorizontal4 from "../../../Image/img-horizontal-4.png"; 
 import "swiper/swiper.scss"; 
 import "swiper/components/navigation/navigation.min.css";
 import "./index.scss";
@@ -17,9 +11,6 @@ function HorizontalSlide () {
   return (
     <div className="learn-coding">
       <h3 className="learn-coding__title"><FormattedMessage id="ID_ABOUT_HORIZONTAL_SLIDE_TITLE" /></h3>
-      <img src={testImg}/>
-      <img src={ImgVertical1} />
-
       <div className="learn-coding__slide-wrap">
         <Swiper navigation={true} 
                 modules={[Navigation]} 
@@ -30,7 +21,7 @@ function HorizontalSlide () {
                 className="learn-coding__slide">
           <SwiperSlide>
             <div className="learn-coding__item">
-              <img alt="슬라이드 아이템 이미지 1" src={testImg} />
+              <img alt="슬라이드 아이템 이미지 1" src={ImgHorizontal1} />
               <h3 className="learn-coding__item-title"><FormattedMessage id="ID_ABOUT_HORIZONTAL_SLIDE_ITEM_TITLE_1" /></h3>
               <p className="learn-coding__item-content"><FormattedMessage id="ID_ABOUT_HORIZONTAL_SLIDE_ITEM_CHILD_TITLE_1" /></p>
             </div>
