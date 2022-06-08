eef8c8ccacf814439864eaf2f6a956ef80f7d62e
jimmy <jimmy@wizschool.io>
2022-06-08 18:23:13 +0900

    add Contact

--- src/Page/Intro/Contact/index.js
+++ src/Page/Intro/Contact/index.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { useState, useEffect, useRef } from "react";
 import { FormattedMessage } from "react-intl";
 import DownArrowIcon from "../../../Image/ic_dropdown_down.svg";
 import BackIcon from "../../../Image/inc_arrow_left.svg";
 import "./index.scss";
+import * as request from "../../../Common/Util/HTTPRequest";
+import { URL } from "../../../Common/Util/HTTPRequest";
 
-function Contact () {
+
+
+let institutionName = "";
+let enrollStudents = "";
+let note = "";
+
+const Contact = () => {
+  const [viewIndex,setViewIndex] = useState(1)
+
   return (
     <div className="contact">
       <div className="contact__inner">
@@ -15,7 +26,7 @@ function Contact () {
 
         <div className="contact__form">
           <div className="contact__from-inner">
-            { false ? <FormFirst /> : <FormSecond /> }
+            { viewIndex < 2 ? <FormFirst viewIndex={viewIndex} setViewIndex={setViewIndex}/> : <FormSecond viewIndex={viewIndex} setViewIndex={setViewIndex} /> }
           </div>
         </div>
       </div>
@@ -26,7 +37,36 @@ function Contact () {
 export default Contact;
 
 /* TODO 첫번째 form */
-const FormFirst = () => {
+const FormFirst = (props) => {
+
+  const [enableBtn,setEnableBtn] = useState(false)
+
+  const onChange = e => {
+    const { value, id } = e.target;
+
+    switch(id) {
+      case 'institutionName':
+        institutionName = value
+        if(institutionName.length > 1) {
+          setEnableBtn(true)
+        }
+        break
+      case 'enrollStudents':
+        enrollStudents = value
+        break
+      case 'note':
+        note = value
+        break
+    }
+
+    console.log(institutionName,enrollStudents,note)
+  }
+
+  const onClickNextBtn = ()=> {
+    props.setViewIndex(2)
+    console.log(11111111111)
+  }
+
   return (
     <>
       <div className="contact__form-sort">
@@ -39,23 +79,23 @@ const FormFirst = () => {
         <div className="contact__form-content">
           {/* TODO 필수 항목인 경우 span 에 클래스 required 추가 */}
           <span className="contact__form-input-title required">Institution Name</span>
-          <input type="text" className="contact__form-input"/>
+          <input id="institutionName" type="text" className="contact__form-input" onChange={onChange}/>
         </div>
 
         <div className="contact__form-content">
           <span className="contact__form-input-title">How many students will be enrolled in?</span>
-          <input type="text" className="contact__form-input"/>
+          <input id="enrollStudents" type="text" className="contact__form-input" onChange={onChange}/>
         </div>
 
         <div className="contact__form-content">
           <span className="contact__form-input-title">Any other note?</span>
-          <textarea className="contact__form-textarea" />
+          <textarea id="note" className="contact__form-textarea" onChange={onChange}/>
         </div>
       </div>
 
       <div className="contact__form-bottom">
         {/* TODO 활성화 시 클래스 active 추가 */}
-        <button type="button" className="contact__from-next">Next</button>
+        <button type="button" className={`contact__from-next ${enableBtn ? "active" : ""}`} onClick = {onClickNextBtn}>Next</button>
       </div>
 
     </>
@@ -63,7 +103,48 @@ const FormFirst = () => {
 }
 
 /* TODO 두번째 form */
-const FormSecond = () => {
+const FormSecond = (props) => {
+
+  let familyName = ""
+  let givenName = ""
+  let title = ""
+  let phone = ""
+  let email = ""
+
+  const onClickBtns = (id)=> {
+    switch(id) {
+      case 'backBtn':
+        props.setViewIndex(1)
+        break
+      case 'sendBtn':
+        request.sendContact({ "familyName": "Danny", "givenName": "Lee",
+        "title": "Hello", "phone": "+821076645035", "email":"danny@redbrick.space",
+        "institutionName": "Monster Inc.", "enrollStudents": "5",
+        "note": "Hello World"})
+        break
+    }
+  }
+
+  const onChange = e => {
+    const { value, id } = e.target;
+
+    console.log(value,id)
+    switch(id) {
+      case 'familyName':
+        familyName = value
+        break
+      case 'givenName':
+        givenName = value
+        break
+      case 'phone':
+        phone = value
+        break
+      case 'email':
+        email = value
+        break
+    }
+  }
+
   return (
     <>
       <div className="contact__form-sort">
@@ -77,13 +158,13 @@ const FormSecond = () => {
           <div className="contact__form-content contact__form-content--name"> 
             {/* TODO 필수 항목인 경우 span 에 클래스 required 추가 */}
             <span className="contact__form-input-title required">First Name</span>
-            <input type="text" className="contact__form-input"/>
+            <input id="familyName" type="text" className="contact__form-input" onChange={onChange}/>
           </div>
 
           <div className="contact__form-content contact__form-content--name"> 
             {/* TODO 필수 항목인 경우 span 에 클래스 required 추가 */}
             <span className="contact__form-input-title required">Last Name</span>
-            <input type="text" className="contact__form-input"/>
+            <input id="givenName" type="text" className="contact__form-input" onChange={onChange}/>
           </div>
         </div>
 
@@ -129,23 +210,23 @@ const FormSecond = () => {
                 </ul>
               </div>
             </div>
-            <input type="text" className="contact__form-input"/>
+            <input id="phone" type="text" className="contact__form-input" onChange={onChange}/>
           </div>
         </div>
 
         <div className="contact__form-content contact__form-content--name"> 
           {/* TODO 필수 항목인 경우 span 에 클래스 required 추가 */}
           <span className="contact__form-input-title required">Email</span>
-          <input type="text" className="contact__form-input"/>
+          <input id="email" type="text" className="contact__form-input" onChange={onChange}/>
         </div>
 
         <div className="contact__form-bottom contact__form-bottom--flex">
-          <button type="button" className="contact__form-back">
+          <button type="button" className="contact__form-back" onClick={()=>onClickBtns("backBtn")}>
             <img alt="뒤로가기 버튼" src={BackIcon} />
             back
           </button>
           {/* TODO 활성화 시 클래스 active 추가 */}
-          <button type="button" className="contact__from-send-btn">Send Message</button>
+          <button type="button" className="contact__from-send-btn active" onClick={()=>onClickBtns("sendBtn")}>Send Message</button>
         </div>
       </div>
     </>
