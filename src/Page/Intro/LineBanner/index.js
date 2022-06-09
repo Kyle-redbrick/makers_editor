5a081abfe2465431bd7e4ccd22c4c107066df40e
haL-redbrick <hal@redbrick.space>
2022-06-09 14:34:39 +0900

    라인배너 링크 이동 연동

--- src/Page/Intro/LineBanner/index.js
+++ src/Page/Intro/LineBanner/index.js
@@ -11,7 +11,7 @@ function LineBanner () {
         <span className="line-banner__child-title"><FormattedMessage id="ID_INTRO_LINE_BANNER_CHILD_TITLE" /></span>
 
         {/* TODO  클릭 시 무료 체험용 과정 페이지로 랜딩 됩니다. */}
-        <a  className="line-banner__go-to-link" rel="noopener noreferrer" ><FormattedMessage id="ID_INTRO_LINE_BANNER_GO_TO_FREE_COURSE_LINK" />
+        <a  className="line-banner__go-to-link" href="https://astroboy-dev.wizclass.com/learn" rel="noopener noreferrer" ><FormattedMessage id="ID_INTRO_LINE_BANNER_GO_TO_FREE_COURSE_LINK" />
         <img alt="더보기 아이콘" src={LinkArrowRightIcon} /></a>
       </div>
     </div>
