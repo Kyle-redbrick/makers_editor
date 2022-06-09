e238031ed77a4457d3e9de0d422db7809cbd087f
haL-redbrick <hal@redbrick.space>
2022-06-09 15:44:10 +0900

    link 변경

--- src/Page/Intro/LineBanner/index.js
+++ src/Page/Intro/LineBanner/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FormattedMessage } from "react-intl";
 import LinkArrowRightIcon from "../../../Image/btn_arrow_right.svg"
+import { Link } from "react-router-dom"
 import "./index.scss";
 
 function LineBanner () {
@@ -11,8 +12,10 @@ function LineBanner () {
         <span className="line-banner__child-title"><FormattedMessage id="ID_INTRO_LINE_BANNER_CHILD_TITLE" /></span>
 
         {/* TODO  클릭 시 무료 체험용 과정 페이지로 랜딩 됩니다. */}
-        <a  className="line-banner__go-to-link" href="https://astroboy-dev.wizclass.com/learn" rel="noopener noreferrer" ><FormattedMessage id="ID_INTRO_LINE_BANNER_GO_TO_FREE_COURSE_LINK" />
-        <img alt="더보기 아이콘" src={LinkArrowRightIcon} /></a>
+        <Link to="/learn" className="line-banner__go-to-link">
+          <FormattedMessage id="ID_INTRO_LINE_BANNER_GO_TO_FREE_COURSE_LINK" />
+          <img alt="더보기 아이콘" src={LinkArrowRightIcon} />
+        </Link>
       </div>
     </div>
   )
