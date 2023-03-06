7e0439bdeb2bbd1b0ce5848fcc92f2bc5a30870f
rarlala-redbrick <rarla@redbrick.space>
2023-03-06 09:37:17 +0900

    Edit Footer Agreement

--- src/Common/Component/Layout/Footer/index.js
+++ src/Common/Component/Layout/Footer/index.js
@@ -1,70 +1,34 @@
-import React, { Component } from "react";
-import "./index.scss";
-import { injectIntl, FormattedMessage } from "react-intl";
+import React, {Component} from "react";
+import {injectIntl, FormattedMessage} from "react-intl";
+import PopUp, {showPopUp} from "../../PopUp";
 import logo_gray from "../../../../Image/dreamclass/logo_gray.png";
-import Markdown from "react-markdown";
-
-import terms from "../../SignUp/Agreements/terms";
-import terms_jp from "../../SignUp/Agreements/terms-jp";
-import terms_en from "../../SignUp/Agreements/terms-en";
-import privacy from "../../SignUp/Agreements/privacy";
-import privacy_jp from "../../SignUp/Agreements/privacy-jp";
-import privacy_en from "../../SignUp/Agreements/privacy-en";
-import PopUp, { showPopUp } from "../../PopUp";
+import "./index.scss";
 
 class Footer extends Component {
-  constructor(props) {
-    super(props);
-    this.locale = localStorage.getItem("lang");
-    let termDoc = terms;
-    let privacyDoc = privacy;
-
-    if (this.locale === "ja") {
-      privacyDoc = privacy_jp;
-      termDoc = terms_jp;
-    } else if (this.locale === "en") {
-      privacyDoc = privacy_en;
-      termDoc = terms_en;
-    }
-
-    this.agreements = [
-      {
-        id: "Terms",
-        title: this.props.intl.formatMessage({
-          id: "ID_FOOTER_TERMS",
-        }),
-        text: termDoc,
-      },
-      {
-        id: "Privacy",
-        title: this.props.intl.formatMessage({
-          id: "ID_FOOTER_PRIVACY",
-        }),
-        text: privacyDoc,
-      },
-    ];
+  constructor() {
+    super();
+    this.agreements = ["ID_FOOTER_TERMS", "ID_FOOTER_PRIVACY"];
   }
 
-  onClickShowAgreement = (id) => {
-    const agreement = this.agreements.find((agreement) => agreement.id === id);
-    showPopUp(
-      <PopUp.Agreement title={agreement.title} content={agreement.text} />
-    );
+  onClickShowAgreement = type => {
+    showPopUp(<PopUp.Agreement type={type} />);
   };
 
   render() {
-    const { agreements, onClickShowAgreement } = this;
+    const {agreements, onClickShowAgreement} = this;
     return (
       <div className="Footer">
         <div className="Footer--Inner">
           <div className="Footer--Inner__term-row">
-            {agreements.map((agreement, index) => (
+            {agreements.map(agreement => (
               <div
                 className="Agreement"
-                key={index}
-                onClick={() => onClickShowAgreement(agreement.id)}
+                key={agreement}
+                onClick={() => onClickShowAgreement(agreement)}
               >
-                <div className={agreement.id}>{agreement.title}</div>
+                <div className="Footer--Inner__term-item">
+                  <FormattedMessage id={agreement} />
+                </div>
               </div>
             ))}
           </div>
--- src/Common/Component/PopUp/Agreement/index.js
+++ src/Common/Component/PopUp/Agreement/index.js
@@ -1,26 +1,47 @@
-import React, { Component } from "react";
-import "./index.scss";
+import React, {Component} from "react";
+import {FormattedMessage} from "react-intl";
 import Markdown from "react-markdown";
-
+import terms from "../../SignUp/Agreements/terms";
+import terms_jp from "../../SignUp/Agreements/terms-jp";
+import terms_en from "../../SignUp/Agreements/terms-en";
+import privacy from "../../SignUp/Agreements/privacy";
+import privacy_jp from "../../SignUp/Agreements/privacy-jp";
+import privacy_en from "../../SignUp/Agreements/privacy-en";
+import "./index.scss";
 
 class AgreementPopUp extends Component {
-  render() {
-    const {
-      title,
-      content
-    } = this.props;
+  constructor(props) {
+    super(props);
+    this.locale = localStorage.getItem("lang");
+    this.termDoc = terms;
+    this.privacyDoc = privacy;
 
+    if (this.locale === "ja") {
+      this.privacyDoc = privacy_jp;
+      this.termDoc = terms_jp;
+    } else if (this.locale === "en") {
+      this.privacyDoc = privacy_en;
+      this.termDoc = terms_en;
+    }
+
+    this.agreements = {
+      ID_FOOTER_TERMS: this.termDoc,
+      ID_FOOTER_PRIVACY: this.privacyDoc,
+    };
+  }
+
+  render() {
+    const {type} = this.props;
     return (
       <div className="agreement">
         <div className="popup_title">
-          {title}
+          <FormattedMessage id={type} />
         </div>
         <div className="agreement_text">
-          <Markdown source={content} />
+          <Markdown source={this.agreements[type]} />
         </div>
       </div>
     );
   }
-
 }
 export default AgreementPopUp;
