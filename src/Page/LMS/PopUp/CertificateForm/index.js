c52e3e47938672da47a293b4eede64068a55173c
Christina <93960807+ChrisK007@users.noreply.github.com>
2023-03-27 17:42:53 +0900

    change image && set max characters (#291)
    Co-authored-by: christina <christina@redbrick.space>

--- src/Page/LMS/PopUp/CertificateForm/index.js
+++ src/Page/LMS/PopUp/CertificateForm/index.js
@@ -2,7 +2,7 @@ import React, { useRef, useState } from "react";
 import { injectIntl } from "react-intl";
 import "./index.scss"
 import { showPopUp } from "../../../../Common/Component/PopUp";
-import clearImg from "../../../../Image/quest-clear-img-astroboy.svg"
+import clearImg from "../../../../Image/quest-clear-img-astroboy-2.png"
 
 import Print from "../../../../Common/Component/OCPCertification";
 
@@ -45,8 +45,9 @@ function CertificateForm(props) {
         <div className="questClear_certificate_row">
           <label htmlFor="certificate_name" className="questClear_certificate_label">Name</label>
           <input type="text" id="certificate_name" className="questClear_certificate_field"
-            value={name} onChange={e => setName(e.target.value)} ref={nameInput}>
+            value={name} onChange={e => setName(e.target.value)} ref={nameInput} maxLength={14}>
           </input>
+          <span className="questClear_certificate_field_max_length">{name.length} / 14</span>
         </div>
       </div>
       <div className="questClear_warningmsg">{warning}</div>
